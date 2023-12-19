7e7651243a7072c50aabf7667aa0ded4a7f3b4ba
Fatima Zaidi <f.zaidi182@gmail.com>
2023-12-19 12:12:17 +0000

    commit to git

---
+++ client/app.js
@@ -0,0 +1,19 @@
+console.log("test");
+
+// what do we want to achieve
+// we want to call the api
+// get the response message
+//put my message on the page
+
+// so how do we call an api - with fetch
+
+async function getMessage() {
+  const response = await fetch("http://localhost:8080/message");
+  // after we got the response from the api with fetch, we gonna turn it into json
+  const json = await response.json();
+  // now display message with DOM maniuplation
+  const p = document.getElementById("message");
+  p.textContent = json.message;
+}
+
+getMessage();
